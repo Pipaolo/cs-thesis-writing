0950d864ca3686b5c8311298a0859361dc3b534b
PaoloTolentino <tolentinopaolomatthew23@gmail.com>
2023-05-24 20:34:00 +0800

    removed placeholder code

--- course-explorer/src/server/api/root.ts
+++ course-explorer/src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { courseRouter } from "./routers/course";
 
 /**
@@ -8,7 +7,6 @@ import { courseRouter } from "./routers/course";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   course: courseRouter,
 });
 
