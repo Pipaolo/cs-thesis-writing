8e84176ddddc6795357617d7f39b88559610ca30
PaoloTolentino <tolentinopaolomatthew23@gmail.com>
2023-05-25 22:18:37 +0800

    updated database url

--- course-platform/api/database.py
+++ course-platform/api/database.py
@@ -3,15 +3,17 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Create the engine
-engine = create_engine('mysql://qqx00l5gst8s8i2j6rtu:pscale_pw_fPuWxIpHh90yya2MzU6Ny6VNLGMrlGF6qWnBcz1kNEc@aws.connect.psdb.cloud/course-explorer?ssl=true')
-db_session  = scoped_session(sessionmaker(autocommit=False,
-                                          autoflush=False,
-                                          bind=engine))
+engine = create_engine(
+    "mysql://u2t2n0grulj3big2f3zp:pscale_pw_INtwXiK1xDsE1eKAIMT20jZjL93u8RdZEvTDVoFWQkb@aws.connect.psdb.cloud/course-explorer?ssl=true"
+)
+db_session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+)
 Base = declarative_base()
 Base.query = db_session.query_property()
 
 
 def init_db():
     import models
-    Base.metadata.create_all(bind=engine)
 
+    Base.metadata.create_all(bind=engine)
