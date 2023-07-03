dc5048b2ec51d20546a8d968298e6730e93cea8e
PaoloTolentino <tolentinopaolomatthew23@gmail.com>
2023-07-04 00:26:38 +0800

    hotfix: fixed not searching

--- course-explorer/src/features/home/components/HomeHeader.tsx
+++ course-explorer/src/features/home/components/HomeHeader.tsx
@@ -1,12 +1,13 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
 import { courseSearchAtom } from "@/src/pages";
-import { UserButton } from "@clerk/nextjs";
-import { useAtom, useAtomValue } from "jotai";
-import { Search, SearchIcon } from "lucide-react";
+import { useAtomValue } from "jotai";
+import { SearchIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { HomeSearchFormSchema } from "../types";
 import { useRouter } from "next/router";
-import { Button, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 interface Props {
   isLoading: boolean | undefined;
@@ -14,7 +15,6 @@ interface Props {
 const HomeHeader = (props: Props) => {
   const router = useRouter();
 
-  const { isLoading = false } = props;
   const searchTerm = useAtomValue(courseSearchAtom);
   const { register, handleSubmit } = useForm<HomeSearchFormSchema>({
     resolver: zodResolver(HomeSearchFormSchema),
@@ -26,6 +26,8 @@ const HomeHeader = (props: Props) => {
   const onSubmit = (data: HomeSearchFormSchema) => {
     const { query } = data;
     const path = query ? `/?q=${query}` : "/";
+    console.log("Submitting", data);
+
     void router.push(path, undefined, {
       shallow: true,
     });
@@ -34,7 +36,7 @@ const HomeHeader = (props: Props) => {
   return (
     <div className="flex w-full flex-col items-center justify-center space-y-4">
       <form
-        onSubmit={void handleSubmit(onSubmit, console.error)}
+        onSubmit={handleSubmit(onSubmit, console.error)}
         className="flex w-full space-x-4"
       >
         <InputGroup>
